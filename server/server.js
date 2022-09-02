acacf23315023e00083ea17b15b7c9517b6230bf
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-09-01 23:45:14 -0400

    Fixing for Deployment 2ndt

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require("cors")
-// const path = require('path')
+const path = require('path')
 const app = express()
 const {PORT} = process.env.PORT || 5000
 
@@ -10,9 +10,9 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('client'))
 
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../client/index.html'))
-// })
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 const { upComment } = require('./controller')
 
 app.post('/comment', upComment)
