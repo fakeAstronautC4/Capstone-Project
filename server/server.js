3516acf77d764d8315303c1172cef5ca133ba579
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-09-01 23:52:44 -0400

    Fixing for Deployment 3rd

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
 const express = require('express');
 const cors = require("cors")
 const path = require('path')
 const app = express()
-const {PORT} = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 
 app.use(express.json())
