6c4bb0e2ab037a33e160a40c63c1eee55656733a
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-09-02 00:14:10 -0400

    Fixing for Deployment 5th

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors")
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 
 app.use(express.json())
@@ -16,5 +16,5 @@ const { upComment } = require('./controller')
 
 app.post('/comment', upComment)
 
-app.listen(PORT, () => console.log(`Server is live on ${PORT}`))
+app.listen(port, () => console.log(`Server is live on ${port}`))
 
