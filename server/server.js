6141cb9a9eda303c8a4c63fb26149102c4d2c8f4
fakeAstronautC4 <alonsocamilo609@gmail.com>
2022-09-01 23:42:42 -0400

    Fixing for Deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require("cors")
-const path = require('path')
+// const path = require('path')
 const app = express()
-const PORT = 5000;
+const {PORT} = process.env.PORT || 5000
 
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('client'))
 
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, '../client/index.html'))
+// })
 const { upComment } = require('./controller')
 
 app.post('/comment', upComment)
